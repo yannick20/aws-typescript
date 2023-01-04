8cad783946c106819dee49c2adaba14b6e79906a
Potter <37401491+yannick20@users.noreply.github.com>
2023-01-05 00:15:55 +0100

    add 1

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", (request: Request, response: Response) => {
+    response.statusCode = 200;
+    console.log("Started the server");
+    response.send({"greeting": "Hello World"})
+});
+
+app.get("/ping", (request: Request, response: Response) => {
+    response.statusCode = 200;
+    response.send("Hello Typescript");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Started listening on ${PORT}`);
+});
